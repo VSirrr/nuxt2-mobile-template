1c4d43aa4bec12207287ccaa6f6c09a4a97568b0
魏建军 <weijj@yiche.com>
2023-12-04 20:15:12 +0800

    update

--- components/v-button.vue
+++ components/v-button.vue
@@ -1,104 +1,109 @@
 <template>
-	<button
-		class="v-button"
-		:class="{
-			primary: type === 'primary',
-			warnning: type === 'warnning',
-			default: type === 'default',
-			disabled: type === 'disabled',
-			inactive: inactive,
-			round: round,
-			inline: inline,
-			small: size === 'sm',
-			middle: size === 'md',
-		}"
-		:disabled="type === 'disabled'"
-		@click="$emit('click')"
-	>
-		<slot />
-	</button>
+  <button
+    class="v-button"
+    :class="{
+      round: round,
+      inline: inline,
+      inactive: inactive,
+      disabled: disabled,
+      small: size === 'sm',
+      middle: size === 'md',
+      primary: type === 'primary',
+      default: type === 'default',
+      warnning: type === 'warnning',
+    }"
+    :disabled="disabled"
+    @click="$emit('click')"
+  >
+    <slot />
+  </button>
 </template>
 
 <script>
 export default {
-	name: 'v-button',
-	props: {
-		type: {
-			type: String,
-			default: 'primary',
-		},
-		inactive: {
-			type: Boolean,
-			default: false,
-		},
-		round: {
-			type: Boolean,
-			default: false,
-		},
-		inline: {
-			type: Boolean,
-			default: false,
-		},
-		size: {
-			type: String,
-			default: 'lg',
-		},
-	},
+  name: "v-button",
+  props: {
+    type: {
+      type: String,
+      default: "primary",
+    },
+    inactive: {
+      type: Boolean,
+      default: false,
+    },
+    round: {
+      type: Boolean,
+      default: false,
+    },
+    inline: {
+      type: Boolean,
+      default: false,
+    },
+    size: {
+      type: String,
+      default: "lg",
+    },
+  },
+  computed: {
+    disabled() {
+      return this.type === "disabled";
+    },
+  },
 };
 </script>
 
 <style>
 .v-button {
-	padding: 0 20px;
-	width: 100%;
-	height: 40px;
-	border: 1px solid;
-	border-radius: 4px;
-	outline: none;
-	color: #fff;
-	background-color: #fff;
+  padding: 0 20px;
+  width: 100%;
+  height: 40px;
+  border: 1px solid;
+  border-radius: 4px;
+  outline: none;
+  color: #fff;
+  background-color: #fff;
 }
 
 .v-button.default {
-	color: #313233;
-	border-color: #ccc;
-	background-color: #fff;
+  color: #313233;
+  border-color: #ccc;
+  background-color: #fff;
 }
 
 .v-button.primary {
-	border-color: #2f86f6;
-	background-color: #2f86f6;
+  border-color: #2f86f6;
+  background-color: #2f86f6;
 }
 
 .v-button.warnning {
-	border-color: #ff5257;
-	background-color: #ff5257;
+  border-color: #ff5257;
+  background-color: #ff5257;
 }
 
 .v-button.disabled {
-	border-color: #e2e4ea;
-	background-color: #e2e4ea;
+  border-color: #e2e4ea;
+  background-color: #e2e4ea;
 }
 
 .v-button.round {
-	border-radius: 20px;
+  border-radius: 20px;
 }
 
 .v-button.inline {
-	width: auto;
+  width: auto;
 }
 
 .v-button.small {
-	padding: 0 10px;
-	height: 28px;
+  padding: 0 10px;
+  height: 28px;
 }
 
 .v-button.middle {
-	padding: 0 14px;
-	height: 34px;
+  padding: 0 14px;
+  height: 34px;
 }
 
 .v-button.inactive {
-	opacity: 0.6;
+  opacity: 0.6;
 }
 </style>
